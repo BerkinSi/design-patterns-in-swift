aea8d29f859146fc856e22210276fd2ffc5114b1
berkinsili@gmail.com <berkinsili@Berkins-MacBook-Pro.local>
2019-10-22 14:53:06 +0300

    Singleton initated

---
+++ RWBlueLibrary-Part1-Starter/RWBlueLibrary/LibraryAPI.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryAPI.swift
+//  RWBlueLibrary
+//
+//  Created by Berkin Sili on 22.10.2019.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+final class LibraryAPI {
+  // The shared static constant approach gives other objects access to the singleton object LibraryAPI.
+  static let shared = LibraryAPI()
+  // The private initializer prevents creating new instances of LibraryAPI from outside.
+  private init() {
+    
+  }
+}
